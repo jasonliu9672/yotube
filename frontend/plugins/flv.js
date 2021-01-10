bd9a6d89de348539108bfeeaada5a713d8397f17
jasonliu9672 <jasonliu9672@gmail.com>
2021-01-11 01:07:29 +0800

    update streaming feature

---
+++ frontend/plugins/flv.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import flvjs from 'flv.js'
+
+Vue.use(flvjs)
---
+++ frontend/utils/authRequest.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+const service = axios.create({
+    baseURL: process.env.dataServerUrl,
+    timeout: 500000,
+    withCredentials: true
+})
+
+service.interceptors.request.use(
+    config => {
+        if (window.$nuxt.$store.getters['user/token']) {
+             service.defaults.headers.common['Authorization'] = window.$nuxt.$store.getters['user/token'];
+             console.log('yes')
+        }
+        return config
+    },
+    error => {
+        console.log(error)
+        return new Promise(function (resolve, reject) {
+            if (error.status === 401 && error.config && !error.config.__isRetryRequest) {
+                window.$nuxt.$store.dispatch('user/logout');
+                resolve();
+            }
+            reject(error);
+        })
+    }
+)
+
+service.interceptors.response.use(
+    response => {
+        const res = response.data
+
+        // if (res.code !== 20000) {
+        //     return Promise.reject(new Error(res.message || 'Error'))
+        // } else {
+        return res
+        // }
+    },
+    error => {
+        console.log('err' + error)
+        return Promise.reject(error)
+    }
+)
+
+export const deleteAuthHeader = function () {
+    delete service.defaults.headers.common['Authorization']
+}
+export default service
